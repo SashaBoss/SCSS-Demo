1bff703f01e67b27f54d8e1e059160ceb977203b
Aleksandr Maha <maha.aleksandr@apriorit.com>
2018-03-02 13:44:33 +0200

    scss demo items

---
+++ src/SSCSSConfig/SSCSSConfig/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SSCSSConfig.Startup))]
+namespace SSCSSConfig
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
